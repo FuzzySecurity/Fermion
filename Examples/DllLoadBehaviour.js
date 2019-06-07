23476801f7a2ba35b8b82704ddb6da14b87eb499
b33f <ru.boonen@gmail.com>
2019-06-07 21:24:11 +0100

    indentation-fix
    Remember, always Shift+Alt+f ;)

--- Examples/DllLoadBehaviour.js
+++ Examples/DllLoadBehaviour.js
@@ -3,26 +3,26 @@
 //-------------------------//
 
 // Native function pointers
-var pLoadLibraryA = Module.findExportByName('Kernel32.dll','LoadLibraryA')
-var pLoadLibraryW = Module.findExportByName('Kernel32.dll','LoadLibraryW')
-var pLoadLibraryExA = Module.findExportByName('Kernel32.dll','LoadLibraryExA')
-var pLoadLibraryExW = Module.findExportByName('Kernel32.dll','LoadLibraryExW')
-var pLdrLoadDll = Module.findExportByName('ntdll.dll','LdrLoadDll')
+var pLoadLibraryA = Module.findExportByName('Kernel32.dll', 'LoadLibraryA')
+var pLoadLibraryW = Module.findExportByName('Kernel32.dll', 'LoadLibraryW')
+var pLoadLibraryExA = Module.findExportByName('Kernel32.dll', 'LoadLibraryExA')
+var pLoadLibraryExW = Module.findExportByName('Kernel32.dll', 'LoadLibraryExW')
+var pLdrLoadDll = Module.findExportByName('ntdll.dll', 'LdrLoadDll')
 
 // Enums
 var Dll_dwFlags = {
-    NONE : 0x0,
-    DONT_RESOLVE_DLL_REFERENCES : 0x1,
-    LOAD_IGNORE_CODE_AUTHZ_LEVEL : 0x10,
-    LOAD_LIBRARY_AS_DATAFILE : 0x2,
-    LOAD_LIBRARY_AS_DATAFILE_EXCLUSIVE : 0x40,
-    LOAD_LIBRARY_AS_IMAGE_RESOURCE : 0x20,
-    LOAD_LIBRARY_SEARCH_APPLICATION_DIR : 0x200,
-    LOAD_LIBRARY_SEARCH_DEFAULT_DIRS : 0x1000,
-    LOAD_LIBRARY_SEARCH_DLL_LOAD_DIR : 0x100,
-    LOAD_LIBRARY_SEARCH_SYSTEM32 : 0x800,
-    LOAD_LIBRARY_SEARCH_USER_DIRS : 0x400,
-    LOAD_WITH_ALTERED_SEARCH_PATH : 0x8
+    NONE: 0x0,
+    DONT_RESOLVE_DLL_REFERENCES: 0x1,
+    LOAD_IGNORE_CODE_AUTHZ_LEVEL: 0x10,
+    LOAD_LIBRARY_AS_DATAFILE: 0x2,
+    LOAD_LIBRARY_AS_DATAFILE_EXCLUSIVE: 0x40,
+    LOAD_LIBRARY_AS_IMAGE_RESOURCE: 0x20,
+    LOAD_LIBRARY_SEARCH_APPLICATION_DIR: 0x200,
+    LOAD_LIBRARY_SEARCH_DEFAULT_DIRS: 0x1000,
+    LOAD_LIBRARY_SEARCH_DLL_LOAD_DIR: 0x100,
+    LOAD_LIBRARY_SEARCH_SYSTEM32: 0x800,
+    LOAD_LIBRARY_SEARCH_USER_DIRS: 0x400,
+    LOAD_WITH_ALTERED_SEARCH_PATH: 0x8
 }
 
 // Helpers
@@ -54,44 +54,44 @@ function ParseDllFlags(FlagVal) {
 
 // Hook
 Interceptor.attach(pLoadLibraryA, {
-	onEnter: function (args) {
-		send("[+] Called LoadLibraryA")
+    onEnter: function (args) {
+        send("[+] Called LoadLibraryA")
         send("    |-> LPCSTR: " + args[0].readAnsiString() + "\n")
-	}
+    }
 });
 
 Interceptor.attach(pLoadLibraryW, {
-	onEnter: function (args) {
-		send("[+] Called LoadLibraryW")
+    onEnter: function (args) {
+        send("[+] Called LoadLibraryW")
         send("    |-> LPCWSTR: " + args[0].readUtf16String() + "\n")
-	}
+    }
 });
 
 Interceptor.attach(pLoadLibraryExA, {
-	onEnter: function (args) {
+    onEnter: function (args) {
         var FlagVals = ParseDllFlags(args[2])
-		send("[+] Called LoadLibraryExA")
+        send("[+] Called LoadLibraryExA")
         send("    |-> LPCSTR  : " + args[0].readAnsiString())
         send("    |-> dwFlags : " + FlagVals)
-	}
+    }
 });
 
 Interceptor.attach(pLoadLibraryExW, {
-	onEnter: function (args) {
+    onEnter: function (args) {
         var FlagVals = ParseDllFlags(args[2])
-		send("[+] Called LoadLibraryExW")
+        send("[+] Called LoadLibraryExW")
         send("    |-> LPCWSTR : " + args[0].readUtf16String())
         send("    |-> dwFlags : " + FlagVals)
-	}
+    }
 });
 
 Interceptor.attach(pLdrLoadDll, {
-	onEnter: function (args) {
+    onEnter: function (args) {
         // Store arg pointers to read in onLeave
         // The base address is only populated after the call
         this.NamePtr = args[2];
         this.BasePtr = args[3];
-	},
+    },
 
     onLeave: function (retval) {
         send("[+] Called LdrLoadDll")
